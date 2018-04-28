a98d4dfd7983b170a26331c498103a9d531ea498
kalinin1007 <kalinin1007@gmail.com>
2018-04-28 18:36:09 +0300

    added Excep404

---
+++ Core/Exceptions/Excep404.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Core\Exceptions;
+
+class Excep404 extends \Exception
+{
+    
+}
--- Core/Exceptions/ValidateException.php
+++ Core/Exceptions/ValidateException.php
@@ -2,7 +2,7 @@
 
 namespace Core\Exceptions;
 
-Class ValidateException extends \Exception
+class ValidateException extends \Exception
 {
     public $errors;
 
